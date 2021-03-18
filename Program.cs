695233619fd9fc9da5fe7931c35abc0534cf42ad
ngarvan <ngarvandb@students.amikom.ac.id>
2021-03-18 21:22:05 +0700

    tugas taxi 03

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace taksi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Membuat Object Taxi
+            Taxi taxi = new Taxi();
+
+            // Set Nilai Properties
+            taxi.NamaDriver = "Jono";
+            taxi.DalamTugas = true;
+            taxi.Penumpang = 10;
+
+            // Memanggil Method
+            taxi.TaxiInfo();
+            taxi.JemputPenumpang();
+            taxi.TurunPenumpang();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taksi
+{
+    class Taxi
+    {
+        // Properties
+        public string NamaDriver { get; set; }
+        public bool DalamTugas { get; set; }
+        public int Penumpang { get; set; }
+
+        //Method
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Nama Driver: " + NamaDriver);
+            Console.Write("Dalam Tugas: ");
+            switch (DalamTugas)
+            {
+                case true:
+                    Console.WriteLine("Yes");
+                    break;
+                default:
+                    Console.WriteLine("No");
+                    break;
+            }
+            Console.WriteLine("Nomer penumpang: " + Penumpang + "\n");
+        }
+        public void JemputPenumpang()
+        {
+            Console.WriteLine(NamaDriver+ " sedang menjemput penumpang");
+        }
+        public void TurunPenumpang()
+        {
+            Console.WriteLine(NamaDriver + " selesai mengantar penumpang");
+        }
+    }
+}
